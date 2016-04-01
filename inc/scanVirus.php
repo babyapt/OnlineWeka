1a4d27f2fd8503640ac098de50df3dcb7cc01ed1
babyapt <babyapt@hotmail.com>
2016-04-01 16:19:59 +0700

    Update Virus Scanner File

--- inc/scanVirus.php
+++ inc/scanVirus.php
@@ -1,18 +1,18 @@
 <?php
   session_start();
-  exec(".\ClamAV\clamscan -r --remove C:\\Users\\BabyApt\\OneDrive\\Work\\Weka\\htdocs\\OnlineWeka\\uploads\\".session_id().".arff -l C:\\Users\\BabyApt\\OneDrive\\Work\\Weka\\htdocs\\OnlineWeka\\uploads\\log\\".session_id().".log",$output);
-  for($key=0;$key<=count($output);$key++){
-    if(substr($output[$key],0,1)=='*') continue;
-    else break;
-  }
-  $scanStatus = explode(": ",$output[$key]);
-  $scanStatus = trim($scanStatus[1]);
-  echo $scanStatus;
-  if($scanStatus=='OK'){
+  $virusScanner = '"C:\\Program Files\\Microsoft Security Client\\MpCmdRun.exe"';
+  $fileToScan = "C:\\xampp\\htdocs\\OnlineWeka\\uploads\\".session_id().".arff";
+  //exec(".\ClamAV\clamscan -r --remove C:\\Users\\BabyApt\\OneDrive\\Work\\Weka\\htdocs\\OnlineWeka\\uploads\\".session_id().".arff -l C:\\Users\\BabyApt\\OneDrive\\Work\\Weka\\htdocs\\OnlineWeka\\uploads\\log\\".session_id().".log",$output);
+  $cmd = "$virusScanner -Scan -ScanType 3 -File $fileToScan";
+  exec($cmd,$output);
+  $scanStatus = explode(" ",$output[2]);
+  $scanStatus = $scanStatus[0].$scanStatus[2].$scanStatus[3].$scanStatus[4];
+  if($scanStatus=='Scanningfoundnothreats.'){
     echo 'Pass';
-    echo '<script>parent.scanComplete(1);setTimeout(function(){location.href="?action=preprocessor&noui=true";},100);</script>';
+    echo '<script>parent.scanComplete(1);setTimeout(function(){location.href="?action=preprocessor&noui=true";},1000);</script>';
   } else {
+    unlink($fileToScan);
     echo 'Fail';
-    echo '<script>parent.scanComplete(0);</script>';
+    echo '<script>parent.$("#uploadCancelTab").show();parent.scanComplete(0);</script>';
   }
 ?>
